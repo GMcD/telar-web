a1b8bff2dec5669076403b68462402f5ea08b924
chrisgithub <chris.a.d.kemp@gmail.com>
2021-09-28 12:16:27 +0100

    removed non hmac route

--- micros/collective/router/route.go
+++ micros/collective/router/route.go
@@ -67,7 +67,5 @@ func SetupRoutes(app *fiber.App) {
 	app.Post("/dto/ids", authHMACMiddleware(false), handlers.GetCollectiveByIds)
 	app.Put("/follow/inc/:inc/:collectiveId", authHMACMiddleware(false), handlers.IncreasePostCount)
 	app.Put("/follower/inc/:inc/:collectiveId", authHMACMiddleware(false), handlers.IncreaseFollowerCount)
-
-	// Make a new route to create collectives(how we auth this?..)
-	app.Post("/dto2", append(hmacCookieHandlers, handlers.CreateCollectiveHandle)...)
+	
 }
